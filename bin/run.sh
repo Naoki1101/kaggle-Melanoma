a075bad6158bd74f34ab511b2a3175b62ce92206
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-07-24 23:56:25 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -5,9 +5,10 @@ cd ../src
 # python train.py -m 'resnet18_003' -c 'modify head'
 # python train.py -m 'resnet18_004' -c 'modify head'
 # python train.py -m 'resnet18_005' -c 'modify head'
-python train.py -m 'resnet18_006' -c 'add rotate, cutout'
+# python train.py -m 'resnet18_006' -c 'add rotate, cutout'
 
 # python train.py -m 'se_resnext50_32x4d_001' -c 'base model'
+python train.py -m 'se_resnext50_32x4d_002' -c 'single model'
 
 cd ../
 git add -A
