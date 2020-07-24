e5d757d07c67a085be69f071efad38158d97f65b
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-07-24 23:39:20 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -4,7 +4,7 @@ cd ../src
 # python train.py -m 'resnet18_002' -c 'base model'
 # python train.py -m 'resnet18_003' -c 'modify head'
 # python train.py -m 'resnet18_004' -c 'modify head'
-python train.py -m 'resnet18_005' -c 'FocalLoss'
+python train.py -m 'resnet18_005' -c 'modify head'
 
 # python train.py -m 'se_resnext50_32x4d_001' -c 'base model'
 
--- src/loss.py
+++ src/loss.py
@@ -10,21 +10,3 @@ class BCEWithLogitsLoss(nn.Module):
 
     def forward(self, yhat, y):
         return self.bcel(yhat, y)
-
-
-class FocalLoss(nn.Module):
-    def __init__(self, gamma=0, eps=1e-7):
-        super(FocalLoss, self).__init__()
-        self.gamma = gamma
-        self.eps = eps
-        
-    def forward(self, input, target):
-        logit = F.softmax(input, dim=1)
-        logit = logit.clamp(self.eps, 1. - self.eps)
-        logit_ls = torch.log(logit)
-        loss = F.nll_loss(logit_ls, target, reduction="none")
-        view = target.size() + (1,)
-        index = target.view(*view)
-        loss = loss * (1 - logit.gather(1, index).squeeze(1)) ** self.gamma # focal loss
-
-        return loss.sum()
