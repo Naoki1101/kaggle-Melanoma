6c5f47da9dd9f4d9d5de1283a703f88b5d06b497
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-07-25 09:18:48 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,7 @@
 cd ../src
 
+python train.py -m 'efficientnetb3_001' -c 'size=284'
+
 # python train.py -m 'resnet18_001' -c 'test'
 # python train.py -m 'resnet18_002' -c 'base model'
 # python train.py -m 'resnet18_003' -c 'modify head'
@@ -8,7 +10,7 @@ cd ../src
 # python train.py -m 'resnet18_006' -c 'add rotate, cutout'
 
 # python train.py -m 'se_resnext50_32x4d_001' -c 'base model'
-python train.py -m 'se_resnext50_32x4d_002' -c 'single model'
+# python train.py -m 'se_resnext50_32x4d_002' -c 'size=512'
 
 cd ../
 git add -A
