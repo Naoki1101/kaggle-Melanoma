a55058a9834d891e1b000d616c1f14a61cbaeca0
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-08-03 21:35:08 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -38,13 +38,12 @@ cd ../src
 # python train.py -m 'efficientnetb3_036' -c '384, 1fold, tta=1, Augmix'
 # python train.py -m 'efficientnetb3_037' -c '384, 5fold, tta=10, Augmix'
 # python train.py -m 'efficientnetb3_038' -c '384, 1fold, tta=1, Augmix, target_encoding_size'
+python train.py -m 'efficientnetb3_039' -c '384, 1fold, tta=1, Augmix, rondom crop'
 
 # python train.py -m 'efficientnetb4_001' -c 'external data'
 # python train.py -m 'efficientnetb4_002' -c '5fold, tta=5'
 # python train.py -m 'efficientnetb4_003' -c '512, 5fold, tta=10, WeightedFocalLoss, modify head'
 # python train.py -m 'efficientnetb4_004' -c '384, 1fold, tta=1, Augmix, target_encoding_size'
-
-python train.py -m 'efficientnetb6_001' -c '384, 1fold, tta=1, Augmix, target_encoding_size'
 
 # python train.py -m 'resnet18_001' -c 'test'
 # python train.py -m 'resnet18_002' -c 'base model'
