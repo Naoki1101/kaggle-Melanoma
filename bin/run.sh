25baa2569f47273295434d61be641e041181e515
Naoki1101 <naoki.narimatsu111@gmail.com>
2020-08-08 10:32:55 +0900

    ...

--- bin/run.sh
+++ bin/run.sh
@@ -47,7 +47,7 @@ cd ../src
 # python train.py -m 'efficientnetb3_045' -c '256, 5fold, tta=12, Augmix, epoch=12, seed=2020'
 # python train.py -m 'efficientnetb3_046' -c '384, 5fold, tta=10, modify augmentation, epoch=12, no metadata, seed=2020'
 # python train.py -m 'efficientnetb3_047' -c '384, 5fold, tta=10, epoch=10, no metadata, seed=2021, drop_idx'
-python train.py -m 'efficientnetb3_048' -c '384, 5fold, tta=10, epoch=10, no metadata, seed=42, remove BN'
+# python train.py -m 'efficientnetb3_048' -c '384, 5fold, tta=10, epoch=10, no metadata, seed=42, remove BN'
 
 # python train.py -m 'efficientnetb4_001' -c 'external data'
 # python train.py -m 'efficientnetb4_002' -c '5fold, tta=5'
@@ -87,6 +87,7 @@ python train.py -m 'efficientnetb3_048' -c '384, 5fold, tta=10, epoch=10, no met
 # python ensemble.py -m 'ensemble_012' -c '3models, rank_average'
 # python ensemble.py -m 'ensemble_013' -c '4models, rank_average'
 # python ensemble.py -m 'ensemble_014' -c '5models, rank_average'
+python ensemble.py -m 'ensemble_015' -c '4models, rank_average'
 
 cd ../
 git add -A
